8ae7e2ccec794d63c78c57cd234bfd09ede76396
Gabriel Oporto <gabyoporto87@gmail.com>
2024-02-28 22:44:01 -0300

    Fix squeleton

--- src/app/ui/Skeleton.tsx
+++ src/app/ui/Skeleton.tsx
@@ -1,18 +1,6 @@
 import React from "react";
-import ContentLoader from "react-content-loader";
+const Skeleton = () => {
+  return <div className="w-96 h-96 bg-gray-400 animate-pulse rounded"></div>;
+};
 
-const MyLoader = (props: object) => (
-  <ContentLoader
-    speed={3}
-    width={380}
-    height={380}
-    viewBox="0 0 380 380"
-    backgroundColor="#24242d"
-    foregroundColor="#3d3f4f"
-    {...props}
-  >
-    <rect x="0" y="60" rx="2" ry="2" width="380" height="380" />
-  </ContentLoader>
-);
-
-export default MyLoader;
+export default Skeleton;
