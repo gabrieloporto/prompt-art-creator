5d872e3676e22554b82b72d055989afb05278fcc
Gabriel Oporto <gabyoporto87@gmail.com>
2024-04-10 14:41:54 -0300

    fix AI response

--- src/app/lib/actions.ts
+++ src/app/lib/actions.ts
@@ -8,45 +8,44 @@ export async function createPrediction(
 ): Promise<Prediction> {
   noStore();
 
-  const prediction = await fetch("https://replicate.com/api/predictions", {
-    headers: {
-      accept: "application/json",
-      "accept-language": "es-AR,es;q=0.9,es-419;q=0.8,en;q=0.7",
-      "cache-control": "no-cache",
-      "content-type": "application/json",
-      pragma: "no-cache",
-      "sec-ch-ua":
-        '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": '"Windows"',
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      "x-csrftoken": "IS3lmIUUJH05xr5Vwu3NkqaBEArOXG3I",
-    },
-    referrer: "https://replicate.com/lucataco/sdxl-lightning-4step",
-    referrerPolicy: "same-origin",
-    body: JSON.stringify({
-      input: {
-        width: 1024,
-        height: 1024,
-        prompt: formData.get("prompt") as string,
-        scheduler: "K_EULER",
-        num_outputs: 1,
-        guidance_scale: 0,
-        negative_prompt: "worst quality, low quality, blur",
-        num_inference_steps: 4,
+  const prediction = await fetch(
+    "https://replicate.com/api/models/bytedance/sdxl-lightning-4step/versions/727e49a643e999d602a896c774a0658ffefea21465756a6ce24b7ea4165eba6a/predictions",
+    {
+      headers: {
+        accept: "application/json",
+        "accept-language": "es-AR,es;q=0.9,es-419;q=0.8,en;q=0.7",
+        "cache-control": "no-cache",
+        "content-type": "application/json",
+        pragma: "no-cache",
+        "sec-ch-ua":
+          '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-csrftoken": "IS3lmIUUJH05xr5Vwu3NkqaBEArOXG3I",
       },
-      is_training: false,
-      create_model: "0",
-      stream: false,
-      version:
-        "727e49a643e999d602a896c774a0658ffefea21465756a6ce24b7ea4165eba6a",
-    }),
-    method: "POST",
-    mode: "cors",
-    credentials: "include",
-  }).then((res) => res.json() as Promise<Prediction>);
+      referrer: "https://replicate.com/bytedance/sdxl-lightning-4step",
+      referrerPolicy: "same-origin",
+      body: JSON.stringify({
+        input: {
+          width: 1024,
+          height: 1024,
+          prompt: formData.get("prompt") as string,
+          scheduler: "K_EULER",
+          num_outputs: 1,
+          guidance_scale: 0,
+          negative_prompt: "worst quality, low quality",
+          num_inference_steps: 4,
+        },
+        stream: false,
+      }),
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+    }
+  ).then((res) => res.json() as Promise<Prediction>);
 
   return prediction;
 }
@@ -61,14 +60,14 @@ export async function getPrediction(id: string) {
       "cache-control": "no-cache",
       pragma: "no-cache",
       "sec-ch-ua":
-        '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+        '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
       "sec-ch-ua-mobile": "?0",
       "sec-ch-ua-platform": '"Windows"',
       "sec-fetch-dest": "empty",
       "sec-fetch-mode": "cors",
       "sec-fetch-site": "same-origin",
     },
-    referrer: "https://replicate.com/lucataco/sdxl-lightning-4step",
+    referrer: "https://replicate.com/bytedance/sdxl-lightning-4step",
     referrerPolicy: "same-origin",
     body: null,
     method: "GET",
